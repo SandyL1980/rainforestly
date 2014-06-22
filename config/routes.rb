31a38738f292bdbce06f913cbc1769bcfc15e6b8
Chad <simpleGrowth@users.noreply.github.com>
2014-06-22 19:24:47 -0400

    add uikit framework

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  root :to => "products#index"
+
   resources :products do
     resources :reviews, :only => [:show, :create, :destroy]
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# http://placehold.it/450x450
+
+arr = []
+
+5.times do |product|
+  name =
+  description =
+  price =
+  photo_url =
+  new_product = [name, description, price, photo_url]
+  arr.push(new_product)
+end
+
+add_products = [
+  ["Photo 1", "Artist", "http://lorempixel.com/700/450/nature/1/"],
+  ["Photo 2", "Artist", "http://lorempixel.com/700/450/nature/2/"],
+  ["Photo 3", "Artist", "http://lorempixel.com/700/450/nature/3/"],
+  ["Photo 4", "Artist", "http://lorempixel.com/700/450/nature/4/"],
+  ["Photo 5", "Artist", "http://lorempixel.com/700/450/nature/5/"],
+  ["Photo 6", "Artist", "http://lorempixel.com/700/450/nature/6/"],
+  ["Photo 7", "Artist", "http://lorempixel.com/700/450/nature/7/"],
+  ["Photo 8", "Artist", "http://lorempixel.com/700/450/nature/8/"],
+  ["Photo 9", "Artist", "http://lorempixel.com/700/450/nature/9/"]
+]
+
+add_photos.each do |title, artist, url|
+  Picture.create( :title => title, :artist => artist, :url => url )
+end
